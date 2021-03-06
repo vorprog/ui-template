354ae231740e1f7e1c8dd564274d8fc84fdc10cb
Richard <richardnsnider@gmail.com>
2021-03-05 16:40:36 -0800

    fix routing directory generation

--- generate-routing.sh
+++ generate-routing.sh
@@ -10,10 +10,10 @@ i=0
 echo "<ul>" >> $OUTPUT
 for filepath in `find "$ROOT" -maxdepth 1 -mindepth 1 -not -path '*/\.*' | sort`; do
   path=`basename "$filepath"`
-  echo "<li><a href=\"/$path\" target=\"_blank\">$file</a></li>" >> $OUTPUT
+  echo "<li><a href=\"/$path\" target=\"_blank\">$filepath</a></li>" >> $OUTPUT
   for i in `find "$filepath" -maxdepth 1 -mindepth 1 -type f| sort`; do
     file=`basename "$i"`
-    echo "<li><a href=\"/$path/$file\" target=\"_blank\">$file</a></li>" >> $OUTPUT
+    echo "<li><a href=\"/$path/$file\" target=\"_blank\">$path/$file</a></li>" >> $OUTPUT
   done
 done
 echo "</ul>" >> $OUTPUT
