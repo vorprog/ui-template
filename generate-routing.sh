0743a13e82361c8bb292a8ff1977be83766b8a49
Richard <richardnsnider@gmail.com>
2021-03-05 16:33:20 -0800

    target main index for route directory generation

--- generate-routing.sh
+++ generate-routing.sh
@@ -2,18 +2,18 @@
 
 ROOT="."
 HTTP="/"
-OUTPUT="site-index.html" 
+OUTPUT="index.html" 
+
+echo "<code>wget -qO - richardsnider.github.io/hello-world | bash</code>" > $OUTPUT
 
 i=0
-echo "<ul>" > $OUTPUT
+echo "<ul>" >> $OUTPUT
 for filepath in `find "$ROOT" -maxdepth 1 -mindepth 1 -not -path '*/\.*' | sort`; do
   path=`basename "$filepath"`
   echo "<li><a href=\"/$path\" target=\"_blank\">$file</a></li>" >> $OUTPUT
-  echo "<ul>" >> $OUTPUT
   for i in `find "$filepath" -maxdepth 1 -mindepth 1 -type f| sort`; do
     file=`basename "$i"`
     echo "<li><a href=\"/$path/$file\" target=\"_blank\">$file</a></li>" >> $OUTPUT
   done
-  echo "</ul>" >> $OUTPUT
 done
 echo "</ul>" >> $OUTPUT
