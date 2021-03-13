ec17b6d7f018dedc1a29e65fe4b8575427e94f90
Richard Snider <rsnider@skyslope.com>
2021-03-13 23:32:19 +0000

    separate loop result intention

--- boilerplate/src/utilities/loop.js
+++ boilerplate/src/utilities/loop.js
@@ -1,41 +1,20 @@
-const kvp = require('./keyValuePair');
+const loopToObject = require('./loopToObject');
 
-module.exports = (collection, operation = (key, value) => new kvp(key, value)) => {
-  const objectResult = iterate(collection, operation);
+module.exports = (collection, operation) => {
+  const objectResult = loopToObject(collection, operation);
   const resultKeys = Object.keys(objectResult);
 
-  if (hasAnyNonNumericValue(resultKeys) || (hasNoZeroValue(resultKeys) && !Array.isArray(collection)))
-    return objectResult;
+  if (hasAllNumericValues(resultKeys)) {
+    const arrayResult = Object.values(objectResult);
+    if (typeof collection === `string` && hasAllStringValues(arrayResult))
+      return getStringFromArray(arrayResult);
 
-  const arrayResult = Object.values(objectResult);
-  if (typeof collection === `string` && hasAllStringValues(arrayResult))
-    return getStringFromArray(arrayResult);
-
-  return arrayResult;
-}
-
-const iterate = (collection, operation) => {
-  const isCollectionAnArray = Array.isArray(collection);
-  let completedIterations = 0;
-  const objectResult = {};
-
-  for (const [key, value] of Object.entries(collection)) {
-    const formattedKey = isCollectionAnArray ? parseInt(key) : key;
-    const iterationResult = operation(formattedKey, value, completedIterations);
-
-    if (iterationResult == null) continue;
-
-    if (iterationResult instanceof kvp)
-      objectResult[iterationResult.key] = iterationResult.value;
-    else objectResult[completedIterations] = iterationResult;
-
-    completedIterations++;
+    return arrayResult;
   }
 
   return objectResult;
-};
+}
 
-const hasAnyNonNumericValue = keyArray => keyArray.some(isNaN)
-const hasNoZeroValue = keyArray => keyArray.indexOf(`0`) === -1;
+const hasAllNumericValues = keyArray => keyArray.every(isFinite)
 const hasAllStringValues = array => array.every(value => typeof value === `string`);
 const getStringFromArray = array => array.join(``);
