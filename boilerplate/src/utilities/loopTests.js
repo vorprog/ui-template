245b48b3a8fa62e6df6771a2a722fcacfdb89a0c
Richard Snider <rsnider@skyslope.com>
2021-03-13 16:39:37 +0000

    tweaks to loop code

--- boilerplate/src/utilities/loopTests.js
+++ boilerplate/src/utilities/loopTests.js
@@ -2,30 +2,38 @@ const loop = require('./loop');
 const kvp = require('./keyValuePair');
 
 const evenNumbers = [2, 4, 6, 8, 10];
-const words = ["foo", "bar"];
-const car = {
-  make: 'Tesla',
-  model: 'Cyber Truck',
-  year: '2021',
+const words = ["foo", "bar", "blee", "bla", "meh"];
+
+const car1 = {
+  make: `Tesla`,
+  model: `Cyber Truck`,
+  year: `2021`,
   stock: 100,
   isElectric: true
 };
 
-const arrayResult = loop(evenNumbers, (key, value, completedIterations) => [key, value, completedIterations]);
-console.log(`array result: ${JSON.stringify(arrayResult)}`);
+const car2 = {
+  make: `Ford`,
+  model: `Mustang`,
+  year: `1966`,
+  stock: 7,
+  isElectric: false
+};
 
-const kvpResult = loop(evenNumbers, (key, value) => [key * 5, value]);
-console.log(`kvp array result:`)
-console.log(JSON.stringify(kvpResult));
+const evenNumbersDoubled = loop(evenNumbers, (key, value) => value*2);
+console.log(`even numbers doubled  >>> ${JSON.stringify(evenNumbersDoubled)}`);
 
-const filterArrayResult = loop(evenNumbers, (key, value, completedIterations) => (value > 6 ? { fla: key, whoa: completedIterations, blee: "bla", v: value } : null));
-console.log(`filter array result:`)
-console.log(JSON.stringify(filterArrayResult));
+const sortedArray = loop(evenNumbers, (key, value) => new kvp(evenNumbers.length - (key+1), value));
+console.log(`reverse sorted array  >>> ${JSON.stringify(sortedArray)}`);
 
-const objectResults = loop(car, (key, value, completedIterations) => evenNumbers[completedIterations]);
-console.log(`object result:`)
-console.log(JSON.stringify(objectResults));
+const filteredArray = loop(evenNumbers, (key, value) => (value > 6 ? value : null));
+console.log(`filtered array  >>> ${JSON.stringify(filteredArray)}`);
 
-const kvpObjectResult = loop(car, (key, value) => new kvp(key, value));
-console.log(`kvp object result:`)
-console.log(JSON.stringify(kvpObjectResult));
+const randomWordValues = loop(car1, (key, value, completedIterations) => words[completedIterations]);
+console.log(`random word values >>> ${JSON.stringify(randomWordValues)}`);
+
+const changeValues = loop(car1, key => car2[key]);
+console.log(`changed values >>> ${JSON.stringify(changeValues)}`);
+
+const remapValues = loop(car1, (key, value, completedIterations) => new kvp(words[completedIterations], value));
+console.log(`re-mapped values >>> ${JSON.stringify(remapValues)}`);
