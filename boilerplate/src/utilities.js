c1813a8915eaee0099d6f7bdd961984d6e1baa68
Richard Snider <richardnsnider@gmail.com>
2021-03-18 16:30:31 +0000

    add common utiltiies file

---
+++ boilerplate/src/utilities.js
@@ -0,0 +1,11 @@
+const keyValuePair = require('./utilities/keyValuePair');
+const loop = require('./utilities/loop');
+const loopToObject = require('./utilities/loopToObject');
+const newElement = require('./utilities/newElement');
+
+module.exports = {
+  keyValuePair: keyValuePair,
+  loop: loop,
+  loopToObject: loopToObject,
+  newElement: newElement
+};
