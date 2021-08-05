38b522ce54b99fdc9f0cb5bbfa47d8067b0d55a3
Richard Snider <richardnsnider@gmail.com>
2021-08-05 23:07:20 +0000

    fix banner message typo

--- main.js
+++ main.js
@@ -557,7 +557,7 @@ module.exports = () => ({
 /***/ ((module) => {
 
 module.exports = {
-  bannerMessage: `Hello! Welcome to my github pages website (work in progress)! This UI uses github's web api to help you view files and directories I keep in this repo. I'm not a font-end expert and chose to develop and experiment with my own user interface library rather than just create another react app.`,
+  bannerMessage: `Hello! Welcome to my github pages website (work in progress)! This UI uses github's web api to help you view files and directories I keep in this repo. I'm not a front-end expert and chose to develop and experiment with my own user interface library rather than just create another react app.`,
 };
 
 
