87a69fbc6251f8351d71f357fba07b91ce8dd19e
Richard Snider <richardnsnider@gmail.com>
2021-08-06 02:34:20 +0000

    update footer with github and linkedin

--- boilerplate/src/components/getFooterConfig.js
+++ boilerplate/src/components/getFooterConfig.js
@@ -1,11 +1,18 @@
+const svg = require('./getSvgConfig');
+
 /** 
  * @param {import('../utilities/newElement').ElementConfig} customConfig
  * @returns {import('../utilities/newElement').ElementConfig}
  */
- module.exports = (customConfig = {}) => Object.assign({
+module.exports = (customConfig = {}) => Object.assign({
   id: `footer`,
   class: `grey row`,
   children: [{
-    textContent: `FOOTER`
-  }]
+    children: [
+      { tag: `a`, href: `https://linkedin.com/in/richardsnider`, children: [svg(`linkedin`, { fill: `#BBB` })] },
+      { tag: `a`, href: `https://github.com/richardsnider`, children: [svg(`github`, { fill: `#BBB` })] },
+      { textContent: `Created by Richard Snider` }
+    ]
+  }
+  ]
 }, customConfig);
--- main.js
+++ main.js
@@ -264,18 +264,25 @@ module.exports = (columnName) => ({
 /*!*******************************************!*\
   !*** ./src/components/getFooterConfig.js ***!
   \*******************************************/
-/***/ ((module) => {
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+const svg = __webpack_require__(/*! ./getSvgConfig */ "./src/components/getSvgConfig.js");
 
 /** 
  * @param {import('../utilities/newElement').ElementConfig} customConfig
  * @returns {import('../utilities/newElement').ElementConfig}
  */
- module.exports = (customConfig = {}) => Object.assign({
+module.exports = (customConfig = {}) => Object.assign({
   id: `footer`,
   class: `grey row`,
   children: [{
-    textContent: `FOOTER`
-  }]
+    children: [
+      { tag: `a`, href: `https://linkedin.com/in/richardsnider`, children: [svg(`linkedin`, { fill: `#BBB` })] },
+      { tag: `a`, href: `https://github.com/richardsnider`, children: [svg(`github`, { fill: `#BBB` })] },
+      { textContent: `Created by Richard Snider` }
+    ]
+  }
+  ]
 }, customConfig);
 
 
@@ -544,6 +551,8 @@ module.exports = () => ({
     getSymbolConfig(`edit`, `M7.127 22.564l-7.126 1.436 1.438-7.125 5.688 5.689zm-4.274-7.104l5.688 5.689 15.46-15.46-5.689-5.689-15.459 15.46z`),
     getSymbolConfig(`download`, `M16 11h5l-9 10-9-10h5v-11h8v11zm3 8v3h-14v-3h-2v5h18v-5h-2z`),
     getSymbolConfig(`folder`, `M11 5c-1.629 0-2.305-1.058-4-3h-7v20h24v-17h-13z`),
+    getSymbolConfig(`github`, `M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12`),
+    getSymbolConfig(`linkedin`, `M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z`),
   ]
 });
 
