6511fb72ce44739591c1ed38eba124f8c7e8ab7c
Richard Snider <richardnsnider@gmail.com>
2021-08-16 01:14:28 +0000

    remove github pages content from mirrored repo

--- install/nodejs.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-
-# https://github.com/nodesource/distributions/blob/master/README.md#debinstall
-curl --silent https://deb.nodesource.com/setup_14.x | bash -
-sudo apt-get install -y nodejs
